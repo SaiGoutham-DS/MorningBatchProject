d814762c77007acba37833f769e07058a2fda96c
SaiGoutham-DS <gouthamsai.ds@gmail.com>
2023-03-03 11:24:20 +0530

    code added from Goutham

--- src/org/sample/Java_Project.java
+++ src/org/sample/Java_Project.java
@@ -5,6 +5,8 @@ public class Java_Project {
 	public static void main(String[] args) {
 		System.out.println("Java");
 		System.out.println("Selenium");
+		System.out.println("DataBase");
+		System.out.println("oracle Sql");
 	}
 	
 
